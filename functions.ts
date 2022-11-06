0fa105018cd414ea23ec7045be226bcae84f2a21
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-11-06 12:46:19 +0600

    functions-rest parameters

--- functions.ts
+++ functions.ts
@@ -63,3 +63,17 @@ console.log(addTwo(2,3,4,5,6,7,8,9,0));
 
 
 //#endregion rest-parameters
+
+
+//#region generics-start
+function getItems3<T>(items: T[]): T[]{
+    return new Array<T>().concat(items);
+}
+function getItems<Type>(items: Type[]): Type[]{
+    return new Array<Type>().concat(items);
+}
+let concatResult = getItems([1,2,3,4,5]);
+let concatResult2 = getItems<number>([1,2,3,4,5]);
+let concatString = getItems(["a","b","c","d"]);
+let concatString2 = getItems<string>(["a","b","c","d"]);
+//#endregion generics-end
