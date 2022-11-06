e84c37aa35583f36d901c7f7ae38106c3c50eefd
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-11-06 12:41:18 +0600

    array datatype

--- datatypes.ts
+++ datatypes.ts
@@ -27,3 +27,25 @@ console.log(result);
 let isValid : boolean;
 console.log(isValid);
 //#endregion boolean-end
+
+//#region array-start
+let enplist = [];
+let emplist2 : string[];
+let deplist: Array<string>;
+emplist2 = ["Santosh", "Santosh1", "Santosh3"];
+let numlist : Array<number>;
+numlist = [1,2,3,4,5];
+let result2 = numlist.filter( (num) =>num >2 );
+console.log(result2);
+
+let num = numlist.find( (num) => num === 2 );
+console.log(num);
+let num2 = numlist.find( (num) => num === 7 );
+console.log(num2);
+let emp = emplist2.find( (em) => em === "Santosh");
+console.log(emp);
+
+//let sum6 = numlist.reduce(acc, num) => acc + num);
+//console.log(sum6);
+
+//#endregion array-end
